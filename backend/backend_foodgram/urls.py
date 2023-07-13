83f05b0df76e233f809c9f192544c901d2639fcc
SleekVortex <andretyklclash@gmail.com>
2023-07-14 04:36:03 +0700

    fixed started apps

--- backend/backend_foodgram/urls.py
+++ backend/backend_foodgram/urls.py
@@ -1,4 +1,4 @@
-"""foodgram URL Configuration
+"""backend_foodgram URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
