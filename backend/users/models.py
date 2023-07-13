d29b75ae54a9c59452f7e36b7d0a3699bd624b32
SleekVortex <andretyklclash@gmail.com>
2023-07-14 04:55:37 +0700

    9

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,31 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    first_name = models.CharField(
+        'Имя',
+        max_length=150,
+        null=False,
+    )
+    last_name = models.CharField(
+        'Фамилия',
+        max_length=150,
+        null=True
+    )
+    email = models.EmailField(
+        'Электронная почта',
+        max_length=200,
+        unique=True,
+        db_index=True,
+        null=False,
+    )
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
+
+    def __str__(self):
+        return (
+            f'Пользователь {self.first_name} {self.last_name} '
+            f'({self.username})'
+        )
