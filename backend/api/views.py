de0d083adc3771aea63edbca00e17ad94930d2ea
SleekVortex <andretyklclash@gmail.com>
2023-07-14 05:02:50 +0700

    12

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from recipes.models import (Ingredient,
+                            Tag
+                            )
+from rest_framework import filters, viewsets
 
-# Create your views here.
+from .permissions import ReadOnly
+from .serializers import (IngredientSerializer,
+                          TagSerializer
+                          )
+
+User = get_user_model()
+
+
+class ReadOnlyViewSetBase(viewsets.ReadOnlyModelViewSet):
+    permission_classes = (ReadOnly,)
+    pagination_class = None
+    http_method_names = ['get', ]
+
+
+class TagViewSet(ReadOnlyViewSetBase):
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
+
+
+class IngredientViewSet(ReadOnlyViewSetBase):
+    queryset = Ingredient.objects.all()
+    serializer_class = IngredientSerializer
+    filter_backends = (filters.SearchFilter,)
+    search_fields = (r'^name',)
