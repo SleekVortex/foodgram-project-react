8a625e6d4e33705aa757621aa5ca1f51f7770ba0
SleekVortex <andretyklclash@gmail.com>
2023-07-14 09:54:59 +0700

    20

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -1,3 +1,66 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import (Favorite, Ingredient,
+                     Recipe, RecipeIngredient,
+                     ShoppingCart, Tag
+                     )
+
+
+class RecipeIngredientInline(admin.TabularInline):
+    model = RecipeIngredient
+    min_num = 1
+    extra = 1
+
+
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'author', 'added_to_favorites')
+    list_filter = ('tags', 'name', 'author')
+    search_fields = ('name', 'author__username', 'tags__slug', 'tags__name')
+    filter_horizontal = ('tags',)
+    inlines = (RecipeIngredientInline,)
+
+    @staticmethod
+    def added_to_favorites(self, obj):
+        return obj.favorites.count()
+
+    added_to_favorites.short_description = 'Добавления в избранное'
+
+
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('name', 'color', 'slug')
+    list_editable = ('color', 'slug')
+    search_fields = ('name',)
+    prepopulated_fields = {'slug': ('name',)}
+
+
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = ('name', 'measurement_unit')
+    list_editable = ('measurement_unit',)
+    list_filter = ('name',)
+    search_fields = ('name',)
+
+
+class RecipeIngredientAdmin(admin.ModelAdmin):
+    list_display = ('recipe', 'ingredient', 'amount')
+    list_editable = ('amount',)
+
+
+class FavoriteAdmin(admin.ModelAdmin):
+    list_display = ('id', 'recipe', 'user')
+
+
+class ShoppingCartAdmin(admin.ModelAdmin):
+    list_display = ('id', 'recipe', 'user')
+
+
+register_models = {
+    Recipe: RecipeAdmin,
+    Tag: TagAdmin,
+    Ingredient: IngredientAdmin,
+    RecipeIngredient: RecipeIngredientAdmin,
+    Favorite: FavoriteAdmin,
+    ShoppingCart: ShoppingCartAdmin,
+}
+
+for base_model, admin_model in register_models.items():
+    admin.site.register(base_model, admin_model)
--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import Subscription
+
+User = get_user_model()
+
+
+class CustomUserAdmin(UserAdmin):
+    list_filter = ('email', 'username', 'is_staff', 'is_active')
+    search_fields = ('username', 'email')
+
+
+class SubscriptionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'author', 'subscriber')
+    list_filter = ('author', 'subscriber')
+
+
+register_models = {
+    User: CustomUserAdmin,
+    Subscription: SubscriptionAdmin,
+}
+
+for base_model, admin_model in register_models.items():
+    admin.site.register(base_model, admin_model)
